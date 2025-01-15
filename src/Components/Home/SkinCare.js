79cd25fd755dc6f3846e650b061f9570c5b511ae
Mehedi Hasan <130404728+Mehedi-haasan@users.noreply.github.com>
2025-01-16 02:51:50 +0600

    Update SkinCare.js

--- src/Components/Home/SkinCare.js
+++ src/Components/Home/SkinCare.js
@@ -1,7 +1,7 @@
 import skin from '../Assets/Image/Group 3293.png'
 import Accordion from './Accordion'
 const SkinCare = () => {
-    return <div className='px-5 md:px-10 lg:px-28 xl:px-44 2xl:px-[300px] py-14'>
+    return <div className='px-2 md:px-10 lg:px-28 xl:px-44 2xl:px-[300px] py-14'>
         <div className='grid grid-cols-12 gap-6'>
             <div className='grid col-span-12 md:col-span-6'>
                 <img src={skin} alt='srdgjbsd' className='w-[496px] h-[424px]' />
